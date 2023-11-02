3fb401cbb45c1373f95cc334467b096afeccead7
carlos <carlos.sanabria@upb.edu.co>
2023-11-02 12:53:35 -0500

    fix: correcion con la rama main V_2

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
       //home: const InicioSesion(),
       
       //Ivan
-      home: const misProductos(),
+      home: const MisProductos(),
 
       );
      
