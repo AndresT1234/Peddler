285d7ef3ac3c1c389960135a7f9e337c2bd37aa2
carlos <carlos.sanabria@upb.edu.co>
2023-11-02 14:00:20 -0500

    fix:despliegue vercel

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,9 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
 );
+
 runApp(const MyApp());
+
 }
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
