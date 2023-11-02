104bc13f52d9654b65d13bffbe2b59044ec12aa5
carlos <carlos.sanabria@upb.edu.co>
2023-11-02 13:47:27 -0500

    fix: correcion de archivo pubspec.lock

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
+
 import 'package:peddler/userInterfaz/views/misProductos.dart';
 import 'firebase_options.dart';
 
